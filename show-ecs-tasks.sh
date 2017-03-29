0bbf3e4857bbd6445246f34b91a455fa1164024c
AJ Bowen <aj@convox.com>
2017-03-29 15:16:27 -0500

    Prettier output

--- show-ecs-tasks.sh
+++ show-ecs-tasks.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# $ for cluster in $(aws ecs list-clusters --query clusterArns | tr -d '[]",'); do aws ecs describe-tasks --cluster $cluster --tasks "$(aws ecs list-tasks --cluster $cluster --query 'taskArns' | grep RUNNING -C999)"; done
+
 
 clusters=$(aws ecs list-clusters | jq .clusterArns | tr -d '[]",')
 
 for cluster in $clusters; do
-    echo "*************************************"
     echo "********* CLUSTER: $cluster *********"
-    echo "*************************************"
     echo
-    tasks=$(aws ecs list-tasks --cluster $cluster --query "taskArns")
-    aws ecs describe-tasks \
-        --cluster $cluster \
-        --tasks "$tasks" \
-        --output table
+    task_arns=$(aws ecs list-tasks --cluster $cluster --query "taskArns")
+    results=$(aws ecs describe-tasks \
+                --cluster $cluster \
+                --tasks "$task_arns")
+    failures=$(echo $results | jq .failures)
+    tasks=$(echo "$results" | jq .tasks)
+    echo "Failures:"
+    echo "$failures"
     echo
-    exit
+
+    echo "Tasks:"
+    echo "$tasks" | jq '.[] | .containers[].lastStatus + " " + .containers[].name + " " + .group + " " + .containerInstanceArn' | tr -d '"' | column -t -s " "
+    echo
 done
 
